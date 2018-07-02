b5d4e33ee065f1ba8b7642d730d65de13bec96b9
mmmenno <menno@denengelse.nl>
2018-07-02 14:27:30 +0200

    initial commit

---
+++ artworks.php
@@ -0,0 +1,119 @@
+<?php
+
+$sparqlquery = '
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX hg: <http://rdf.histograph.io/>
+PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX sem: <http://semanticweb.cs.vu.nl/2009/11/sem/>
+PREFIX void: <http://rdfs.org/ns/void#>
+SELECT 	DISTINCT ?cho 
+		(SAMPLE(?img) AS ?img) 
+		(SAMPLE(?spatial) AS ?spatial) 
+		(SAMPLE(?spatialname) AS ?spatialname) 
+		(SAMPLE(?dataset) AS ?dataset) 
+		(SAMPLE(?title) AS ?title) 
+		(SAMPLE(?realbegin) AS ?begin) 
+		(SAMPLE(?maker) AS ?maker) 
+		(SAMPLE(?makeruri) AS ?makeruri) 
+		WHERE {';
+if($_GET['type']!="all"){
+$sparqlquery .= '
+  ?cho dc:type <http://vocab.getty.edu/aat/' . $_GET['type'] . '> .';
+}
+$sparqlquery .= '
+  ?cho void:inDataset ?dataset .
+  ?cho dct:spatial ?spatial .
+  OPTIONAL {
+  	?cho dc:title ?title .
+  }
+  OPTIONAL {
+  	?cho dc:creator ?makeruri .
+  	?makeruri skos:prefLabel ?maker .
+  }
+  ?cho foaf:depiction ?img .
+  OPTIONAL{
+  	?cho sem:hasBeginTimeStamp ?begin .
+  	BIND(IF(COALESCE(xsd:datetime(str(?begin)), \'!\') != \'!\',
+ 		year(xsd:dateTime(str(?begin))),year("1005-01-01"^^xsd:dateTime)) AS ?realbegin ) .
+  }
+  ?spatial skos:prefLabel ?spatialname .
+  FILTER (!REGEX(?wktspatial,"\\\\),\\\\("))
+  ?spatial geo:hasGeometry/geo:asWKT ?wktspatial .
+  bind (bif:st_geomfromtext(?wktspatial) as ?x)
+  bind (bif:st_geomfromtext("POLYGON((' . $_GET['bbox'] . '))") as ?y)
+  FILTER (bif:st_intersects(?x, ?y))
+} 
+GROUP BY ?cho ?realbegin
+ORDER BY ?realbegin
+LIMIT 100';
+
+//echo $sparqlquery;
+
+
+$url = "https://api.data.adamlink.nl/datasets/AdamNet/all/services/endpoint/sparql?default-graph-uri=&query=" . urlencode($sparqlquery) . "&format=application%2Fsparql-results%2Bjson&timeout=120000&debug=on";
+
+$querylink = "https://data.adamlink.nl/AdamNet/all/services/endpoint#query=" . urlencode($sparqlquery) . "&contentTypeConstruct=text%2Fturtle&contentTypeSelect=application%2Fsparql-results%2Bjson&endpoint=https%3A%2F%2Fdata.adamlink.nl%2F_api%2Fdatasets%2Fmenno%2Falles%2Fservices%2Falles%2Fsparql&requestMethod=POST&tabTitle=Query&headers=%7B%7D&outputFormat=table";
+
+
+
+$json = file_get_contents($url);
+
+$data = json_decode($json,true);
+
+$i = 0;
+
+$datasets=array(
+	"https://data.adamlink.nl/iisg/beeldbank/" => "IISG",
+	"https://data.adamlink.nl/am/amcollect/" => "AM",
+	"https://data.adamlink.nl/uva/maps" => "UvA",
+	"https://data.adamlink.nl/saa/beeldbank/" => "SAA",
+	"https://data.adamlink.nl/rma/beeldbank/" => "RMA",
+	"https://data.adamlink.nl/nharchief/beeldbank/" => "NHA"
+);
+
+$names = array();
+$checknames = array();
+$checkimgs = array();
+$imgs = array();
+
+echo "<h2>" . count($data['results']['bindings']) . " resultaten</h2>";
+echo '<div class="row">';
+
+foreach ($data['results']['bindings'] as $row) { 
+	$i++;
+	if($i%2!=0 && $i>1){
+		echo "</div>\n";
+		echo '<div class="row">';
+	}
+	?>
+
+	<div class="col-md-6">
+		<img src="<?= $row['img']['value'] ?>">
+		<strong><?= $row['title']['value'] ?></strong><br />
+		<a target="_blank" href="<?= $row['spatial']['value'] ?>"><strong><?= $row['spatialname']['value'] ?></strong></a> 
+		<?php if($row['begin']['value']>1005){ ?>
+			omstreeks <strong><?= $row['begin']['value'] ?></strong> 
+		<?php } ?>
+		<?php if(isset($row['maker']['value'])){ ?>
+			door <a href="<?= $row['makeruri']['value'] ?>"><strong><?= $row['maker']['value'] ?></strong></a>
+		<?php } ?>
+		<a target="_blank" href="<?= $row['cho']['value'] ?>">bekijk bij <strong><?= $datasets[$row['dataset']['value']] ?></strong></a>
+	</div>
+
+	<?php 
+} 
+
+if($i%2!=0){
+	echo '<div class="col-md-6"></div></div>';
+}else{
+	echo '</div>';
+}
+
+?>
+
+<a target="_blank" style="font-size:36px; margin: 40px 0 40px 0; display: block;" href="<?= $querylink ?>">SPARQL it yourself</a>
+
+
---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>De Verbeelde Stad</title>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,700" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.1.0/dist/leaflet.css" integrity="sha512-wcw6ts8Anuw10Mzh9Ytw4pylW8+NAD4ch3lqm9lzAsTxg0GFeJgoAtxuCLREZSC5lUXdVyo/7yfsqFjQ4S+aKw==" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.1.0/dist/leaflet.js" integrity="sha512-mNqn2Wg7tSToJhvHcqfzLMU6J4mkOImSPTxVZAdo+lcPlk+GhZmYgACEe0x35K7YzW1zJ7XyJV/TT1MrdXvMcA==" crossorigin=""></script>
+
+
+	<style>
+		html, body{
+			height: 100%;
+			margin:0;
+			font-family: 'Nunito', sans-serif;
+		}
+		#parameterbar{
+			background-color: #000;
+			padding-top: 40px;
+			padding-bottom: 40px;
+			color: #fff;
+		}
+		h1{
+			margin-top: 0;
+		}
+		#map {
+			width: 100%;
+			height: 250px;
+		}
+		.leaflet-left .leaflet-control{
+			margin-top: 10px;
+			margin-left: 10px;
+		}
+		.leaflet-container .leaflet-control-attribution{
+			color: #000;
+		}
+		.leaflet-control-attribution a{
+			color: #000;
+		}
+		.leaflet-touch .leaflet-control-layers, .leaflet-touch .leaflet-bar{
+			border: 2px solid #000;
+		}
+		.btn-primary{
+			margin-top: 20px;
+			background-color: #fff;
+			color: #000;
+			font-size: 20px;
+			border-color: #fff;
+		}
+		.btn-primary:hover{
+			background-color: #BC000C;
+			color: #fff;
+			border-color: #fff;
+		}
+		button:focus, input:focus {
+			outline:0;
+		}
+		label{
+			font-weight: 300;
+			margin-left: 16px;
+		}
+		a, a:visited, a:hover, select{
+			text-decoration: none;
+			color: #000;
+		}
+		a:hover{
+			text-decoration: underline;
+		}
+		#album img{
+			width: 100%;
+			margin-top: 20px;
+		}
+	</style>
+
+	
+</head>
+<body>
+
+
+
+<div class="container-fluid" id="parameterbar">
+	<div class="col-md-4">
+		<h1>De Verbeelde Stad</h1>
+		<p>
+			Zoom uit of in op een stukje stad om er afbeeldingen bij op te halen. Standaard krijg je alle typen afbeeldingen door elkaar, maar je kan ook alleen schilderijen, tekeningen, etc. opvragen.
+		</p>
+		<p>
+			We geven maximaal 100 resultaten weer, in dat geval kan je altijd verder inzoomen.
+		</p>
+	</div>
+	<div class="col-md-4">
+		<div id='map'></div>
+	</div>
+	<div class="col-md-4">
+
+		<?php /*
+		<input checked="checked" type="radio" value="300033973" name="techniek" id="tekeningen" /> 
+		<label for="tekeningen">tekeningen</label><br />
+
+		<input type="radio" value="300177435" name="techniek" id="schilderijen" /> 
+		<label for="schilderijen">schilderijen</label><br />
+
+		<input type="radio" value="300127131" name="techniek" id="kabinetfoto" /> 
+		<label for="kabinetfoto">kabinetfoto's</label><br />
+
+		<input type="radio" value="300027221" name="techniek" id="affiches" /> 
+		<label for="affiches">affiches</label><br />
+
+		<input type="radio" value="300041273" name="techniek" id="prenten" /> 
+		<label for="prenten">prenten</label><br />
+
+		<input type="radio" value="300041340" name="techniek" id="gravures" /> 
+		<label for="gravures">gravures</label><br />
+
+		<input type="radio" value="300162872" name="techniek" id="stereo" /> 
+		<label for="stereo">stereofoto's</label><br />
+
+		<input type="radio" value="300026819" name="techniek" id="ansichtkaarten" /> 
+		<label for="ansichtkaarten">ansichtkaarten</label><br />
+
+		<input type="radio" value="300015578" name="techniek" id="illustraties" /> 
+		<label for="illustraties">illustraties</label><br />
+
+		<input type="radio" value="300028051" name="techniek" id="boeken" /> 
+		<label for="boeken">boeken</label><br />
+
+		*/ ?>
+
+		<a id="locate" style="color: #fff; cursor: pointer;">breng kaart naar mijn positie</a><br /><br />
+		
+		<select name="typering" class="form-control">
+			<option value="all">alle typen afbeeldingen</option>
+			<option value="300033973">tekeningen</option>
+			<option value="300177435">schilderijen</option>
+			<option value="300127131">kabinetfoto's</option>
+			<option value="300027221">affiches</option>
+			<option value="300041273">prenten</option>
+			<option value="300041340">gravures</option>
+			<option value="300162872">stereofoto's</option>
+			<option value="300026819">ansichtkaarten</option>
+			<option value="300015578">illustraties</option>
+			<option value="300028051">boeken</option>		
+		</select>
+
+
+		
+		<button id="knop" class="btn btn-primary">zoek</button>
+	</div>
+</div>
+
+<div id="album" class="container-fluid">
+
+</div>
+
+
+
+<script>
+
+	var params = window.location.hash.substr(1);
+	qstring = decodeURIComponent( params );
+	var parsed = function( qstring ) {
+	    var params = {}, queries, temp, i, l;
+	    // Split into key/value pairs
+	    queries = qstring.split("&");
+	    // Convert the array of strings into an object
+	    for ( i = 0, l = queries.length; i < l; i++ ) {
+	        temp = queries[i].split('=');
+	        params[temp[0]] = temp[1];
+	    }
+	    return params;
+	};
+	params = parsed(qstring);
+
+	if(params!=""){
+		if(typeof(params['center']) != "undefined") {
+			var str = params['center'].replace('LatLng(','').replace(')','').replace(' ','');
+			var latlon = str.split(",");
+		    var center = [parseFloat(latlon[0]),parseFloat(latlon[1])];
+		}else{
+			var center = [52.359716,4.900029];
+		}
+		if(typeof(params['zoom']) != "undefined") {
+			var zoomlevel = parseInt(params['zoom']);
+		}else{
+			var zoomlevel = 15;
+		}
+		if(typeof(params['type']) != "undefined") {
+			$("select[name='typering']").val(params['type']);
+		}
+
+	}
+
+	var map = L.map('map', {
+        center: center,
+        zoom: zoomlevel,
+        minZoom: 6,
+        maxZoom: 20,
+        scrollWheelZoom: false
+    });
+
+	L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+	    attribution: 'Tiles <a href="http://stamen.com">Stamen Design</a> - Data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	    subdomains: 'abcd',
+		minZoom: 0,
+		maxZoom: 20,
+		ext: 'png'
+	}).addTo(map);
+
+	L.control.attribution({position: 'bottomleft'}).addTo(map);
+
+	$('#knop').click(function(){
+		getimages();
+	});
+
+	function sethash(){
+		var hash = {};
+		hash['type'] = $("select[name='typering']").val();
+		hash['center'] = map.getCenter();
+		hash['zoom'] = map.getZoom();
+		location.hash = $.param(hash,true);
+	}
+
+	function getimages(){
+		var parameters = {};
+		var bounds = map.getBounds();
+		parameters['bbox'] = bounds['_southWest']['lng'] + ' ' + bounds['_northEast']['lat'];
+		parameters['bbox'] += ',' + bounds['_northEast']['lng'] + ' ' + bounds['_northEast']['lat'];
+		parameters['bbox'] += ',' + bounds['_northEast']['lng'] + ' ' + bounds['_southWest']['lat'];
+		parameters['bbox'] += ',' + bounds['_southWest']['lng'] + ' ' + bounds['_southWest']['lat'];
+		parameters['bbox'] += ',' + bounds['_southWest']['lng'] + ' ' + bounds['_northEast']['lat'];
+		
+		//parameters['type'] = $("input[name='techniek']:checked").val();
+		parameters['type'] = $("select[name='typering']").val();
+		console.log(parameters);
+
+		sethash();
+
+		var params = $.param(parameters,true);
+		var worksurl = 'artworks.php?' + params;
+		
+		$('#album').load(worksurl);
+	}
+
+	$('#locate').click(function() {
+      	map.locate({setView: true, maxZoom: 17});
+    })
+
+	$(document).ready(function(){
+		var check = window.location.hash.substr(1);
+		if(check!=""){
+			getimages();
+		}
+		
+	});
+
+</script>
+
+
+
+</body>
+</html>
